7c79190115d74da9fa6a5ae6ff7152448b057781
Brenda <brentorres26@gmail.com>
2023-06-18 18:26:53 -0300

    ejercicios guia de trabajo n°1

---
+++ unidad1.js
@@ -0,0 +1,39 @@
+//Tarea 1
+//Emplea console.log para enviar nombre completo a la consola.
+console.log("Josefina Duarte");
+
+//Tarea 2
+//Muestra tu año de nacimiento.
+console.log("1970");
+
+//Tarea 3
+//Vuelve a intentar mostrar tu año de nacimiento,
+// esta vez pasando la fecha sin las comillas.
+console.log(1970);
+
+//Tarea 4
+//Podemos pasar varios argumentos a console.log separados por comas, por ejemplo:
+//console.log("abc", "def", "ghi");
+//Envía información sobre ti a la consola en el formato: Nombre Apellido (Año) por ejemplo Mary Stuart (1542).
+//Enviar toda la información como un solo argumento.
+//Enviar la información nombre, apellido, año como argumentos separados.
+
+console.log("Josefina","Duarte",1970);
+
+//Tarea 5
+//Envía la misma información (nombre, apellido, año) a la consola, no una al lado de la otra, 
+//sino en líneas consecutivas.
+console.log("Josefina");
+console.log("Duarte");
+console.log(1970);
+
+//Tarea 6
+//Una cadena se puede concatenar usando el signo +, por ejemplo "abc" + "def" serán tratados como "abcdef". 
+//Intenta escribir tu nombre, apellido y año de nacimiento en una línea nuevamente,
+//esta vez no separados por comas, sino por el signo +.
+console.log("Josefina "+ "Duarte "+ 1970);
+
+//Tarea 7
+//Coloca espacios en los lugares apropiados, de modo que cuando se muestre, 
+//se obtenga el mismo efecto que en la Tarea 4.
+console.log("Josefina "+ "Duarte "+ 1970);
